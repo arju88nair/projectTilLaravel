9c31de6db6738f026e45c797e733326c04228994
arju88nair <arju88nair@gmail.com>
2020-02-12 22:06:46 +0530

    flask scrapy integration 1.1

--- TILREST/main.py
+++ TILREST/main.py
@@ -1,20 +1,67 @@
-from __future__ import unicode_literals
-
 import json
-import requests
-
 from flask import Flask
+from scrapy.crawler import CrawlerRunner
+import os, sys
+from TILScraper.TILScraper.spiders.til_spider import TILSpider
 
-app = Flask(__name__)
+app = Flask('Scrape With Flask')
+crawl_runner = CrawlerRunner()      # requires the Twisted reactor to run
+quotes_list = []                    # store quotes
+scrape_in_progress = False
+scrape_complete = False
 
-@app.route('/')
-def show_quotes():
-    params = {
-        'spider_name': 'til',
-        'start_requests': True
-    }
-    response = requests.get('http://localhost:9080/crawl.json', params)
-    data = json.loads(response.text)
-    result = '\n'.join('<p><b>{}</b> - {}</p>'.format(item['author'], item['text'])
-                       for item in data['items'])
-    return result
+@app.route('/crawl')
+def crawl_for_quotes():
+    """
+    Scrape for quotes
+    """
+    global scrape_in_progress
+    global scrape_complete
+
+    if not scrape_in_progress:
+        scrape_in_progress = True
+        global quotes_list
+        # start the crawler and execute a callback when complete
+        eventual = crawl_runner.crawl(QuoteSpider, quotes_list=quotes_list)
+        eventual.addCallback(finished_scrape)
+        return 'SCRAPING'
+    elif scrape_complete:
+        return 'SCRAPE COMPLETE'
+    return 'SCRAPE IN PROGRESS'
+
+@app.route('/results')
+def get_results():
+    """
+    Get the results only if a spider has results
+    """
+    global scrape_complete
+    if scrape_complete:
+        return json.dumps(quotes_list)
+    return 'Scrape Still Progress'
+
+def finished_scrape(null):
+    """
+    A callback that is fired after the scrape has completed.
+    Set a flag to allow display the results from /results
+    """
+    global scrape_complete
+    scrape_complete = True
+
+
+if __name__=='__main__':
+    from sys import stdout
+    from twisted.logger import globalLogBeginner, textFileLogObserver
+    from twisted.web import server, wsgi
+    from twisted.internet import endpoints, reactor
+    app.run(debug=True)
+    # start the logger
+    globalLogBeginner.beginLoggingTo([textFileLogObserver(stdout)])
+
+    # start the WSGI server
+    root_resource = wsgi.WSGIResource(reactor, reactor.getThreadPool(), app)
+    factory = server.Site(root_resource)
+    http_server = endpoints.TCP4ServerEndpoint(reactor, 9000)
+    http_server.listen(factory)
+
+    # start event loop
+    reactor.run()
