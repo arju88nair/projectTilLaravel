70a259af398755d3f2eab29700f158333891f664
arju88nair <arju88nair@gmail.com>
2020-02-14 23:20:37 +0530

    hello world . Flask initial with Beautifulsoup

--- api/main.py
+++ api/main.py
@@ -1,66 +1,11 @@
-import json
 from flask import Flask
-from scrapy.crawler import CrawlerRunner
-import os, sys
-from scraper.scraper.spiders.urlScraper import TILSpider
 
-app = Flask('Scrape With Flask')
-crawl_runner = CrawlerRunner()      # requires the Twisted reactor to run
-quotes_list = []                    # store quotes
-scrape_in_progress = False
-scrape_complete = False
+app = Flask(__name__)
 
-@app.route('/crawl')
-def crawl_for_quotes():
-    """
-    Scrape for quotes
-    """
-    global scrape_in_progress
-    global scrape_complete
+@app.route('/')
+def index():
+    return "Hello, World!"
 
-    if not scrape_in_progress:
-        scrape_in_progress = True
-        global quotes_list
-        # start the crawler and execute a callback when complete
-        eventual = crawl_runner.crawl(TILSpider, quotes_list=quotes_list)
-        eventual.addCallback(finished_scrape)
-        return 'SCRAPING'
-    elif scrape_complete:
-        return 'SCRAPE COMPLETE'
-    return 'SCRAPE IN PROGRESS'
-
-@app.route('/results')
-def get_results():
-    """
-    Get the results only if a spider has results
-    """
-    global scrape_complete
-    if scrape_complete:
-        return json.dumps(quotes_list)
-    return 'Scrape Still Progress'
-
-def finished_scrape(null):
-    """
-    A callback that is fired after the scrape has completed.
-    Set a flag to allow display the results from /results
-    """
-    global scrape_complete
-    scrape_complete = True
-
-
-if __name__=='__main__':
-    from sys import stdout
-    from twisted.logger import globalLogBeginner, textFileLogObserver
-    from twisted.web import server, wsgi
-    from twisted.internet import endpoints, reactor
-    # start the logger
-    globalLogBeginner.beginLoggingTo([textFileLogObserver(stdout)])
-
-    # start the WSGI server
-    root_resource = wsgi.WSGIResource(reactor, reactor.getThreadPool(), app)
-    factory = server.Site(root_resource)
-    http_server = endpoints.TCP4ServerEndpoint(reactor, 9000)
-    http_server.listen(factory)
-
-    # start event loop
-    reactor.run()
+if __name__ == '__main__':
+    app.config['ENV'] = 'development'
+    app.run(debug=True)
