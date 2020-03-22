bd2b50dbd6a582555297c502dc76635d4ada9745
arju88nair <arju88nair@gmail.com>
2020-03-22 15:58:06 +0530

    list item

--- web/src/views/home/HomeList.js
+++ web/src/views/home/HomeList.js
@@ -1,17 +1,89 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
+import Typography from "@material-ui/core/Typography";
+import LaunchIcon from "@material-ui/icons/Launch";
 
 const useStyles = makeStyles(theme => ({
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
 
+    },
+    image: {
+        width: 128,
+        height: 128,
+    },
+    img: {
+        margin: 'auto',
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '100%',
+    },
+    root: {
+        flexGrow: 1,
+    },
+    count: {
+        marginTop: -10
+    }
 }));
 
-export function LeftPane() {
+export function HomeList() {
     const classes = useStyles();
 
     return (
-        <Grid item xs={9}>
+        <Paper className={classes.paper}>
+            <div className={classes.root}>
+                <Grid container spacing={2} xs={12} sm={12}>
+                    <Grid item xs={1} sm={1}>
+                        <Box display="flex" flexDirection="column" p={1} m={1} justify="center"
+                             alignItems="center">
+                            <Box p={1}>
+                                <ArrowDropUpIcon/>
+                            </Box>
+                            <Box p={1} className={classes.count}>
+                                345
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={11} sm={11} container>
+                        <Grid item xs container
+                              direction="column"
+                              justify="center"
+                              alignItems="flex-start" spacing={2} item xs={11} sm={11}>
+                            <Grid item xs container
+                                  direction="column"
+                                  justify="center"
+                                  alignItems="flex-start">
+                                <Typography gutterBottom style={{
+                                    fontSize: '10',
+                                    fontWeight: 'medium',
+                                    color: '#464855',
+                                    fontFamily: "Poppins"
+                                }}>
+                                    If, like me, you’ve read the Redux docs, watched Dan’s videos, done Wes’ course
+                                    and still not quite grasped how to use Redux,
+                                </Typography>
+                                <Typography variant="body2" gutterBottom>
+                                    added 5 hours ago by lloydyhats in memes
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    284 comments | share | save | hide | report
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={1} sm={1}   alignItems="flex-end"   style={{marginTop:15}} container
+                              direction="column">
+                            <LaunchIcon/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>
+        </Paper>
 
-        </Grid>
     );
 };
--- web/src/views/home/LeftPane.js
+++ web/src/views/home/LeftPane.js
@@ -1,40 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Box from '@material-ui/core/Box';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import {HomeList} from "./HomeList";
 
 const useStyles = makeStyles(theme => ({
-    leftpane: {},
     paper: {
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
+        height: '100vh'
     },
-    image: {
-        width: 128,
-        height: 128,
-    },
-    img: {
-        margin: 'auto',
-        display: 'block',
-        maxWidth: '100%',
-        maxHeight: '100%',
-    },
-    root: {
-        flexGrow: 1,
-    },
-    count:{
-        marginTop:-10
-    }
 }));
 
 export function LeftPane() {
@@ -42,52 +18,7 @@ export function LeftPane() {
 
     return (
         <Grid item xs={9}>
-            <Paper className={classes.paper}>
-            <div className={classes.root}>
-                    <Grid container spacing={2}>
-                        <Grid item>
-                            <Box display="flex" flexDirection="column" p={1} m={1}  justify="center"
-                                 alignItems="center">
-                                <Box p={1}  >
-                                    <ArrowDropUpIcon />
-                                </Box>
-                                <Box p={1}  className={classes.count}>
-                                    345
-                                </Box>
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} sm container>
-                            <Grid item xs  container
-                                  direction="column"
-                                  justify="flex-start"
-                                  alignItems="flex-start" spacing={2}>
-                                <Grid item xs  container
-                                      direction="column"
-                                      justify="flex-start"
-                                      alignItems="flex-start">
-                                    <Typography gutterBottom variant="subtitle1">
-                                        If, like me, you’ve read the Redux docs, watched Dan’s videos, done Wes’ course and still not quite grasped how to use Redux,
-                                    </Typography>
-                                    <Typography variant="body2" gutterBottom>
-                                        If you don’t know what Thunk is, don’t worry too much, but we’ll use it to make asynchronous calls in the “Redux way”.
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary">
-                                        This tutorial assumes you have a basic grasp of React and ES6/2015, but it should hopefully be easy enough to follow along regardless.
-                                    </Typography>
-                                </Grid>
-                                <Grid item>
-                                    <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                                        Remove
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                            <Grid item>
-                                <Typography variant="subtitle1">$19.00</Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-            </div>
-            </Paper>
+            <Paper className={classes.paper}><HomeList/></Paper>
         </Grid>
     );
 };
