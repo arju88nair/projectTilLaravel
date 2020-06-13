720ffa7a626d94c3142259f20c4cd6b8de3b7ec8
arju88nair <arju88nair@gmail.com>
2020-06-13 23:03:08 +0530

    modal

--- web/src/views/Components/CategoryModal.js
+++ web/src/views/Components/CategoryModal.js
@@ -1,55 +1,159 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, {useState} from 'react';
+import {createMuiTheme, makeStyles, ThemeProvider} from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import {useDispatch, useSelector} from "react-redux";
 import {miscActions, userActions} from "../../_actions";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+import {green} from "@material-ui/core/colors/green";
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
     modal: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+
     },
     paper: {
         backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
+            // border: '2px solid #000',
         boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
+        // padding: theme.spacing(2, 4, 3),
     },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        paddingBottom: theme.spacing(2),
+
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+        alignSelf:'right'
+    },
+    modalWindow:{
+        bottom:'35%'
+    }
 }));
+
+const styles = (theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
+});
+const theme = createMuiTheme({
+    palette: {
+        primary: green,
+    },
+});
+
+
+const DialogTitle = withStyles(styles)((props) => {
+    const { children, classes, onClose, ...other } = props;
+    return (
+        <MuiDialogTitle disableTypography className={classes.root} {...other}>
+            <Typography variant="h6">{children}</Typography>
+            {onClose ? (
+                <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </MuiDialogTitle>
+    );
+});
+
+const DialogContent = withStyles((theme) => ({
+    root: {
+        padding: theme.spacing(2),
+    },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(1),
+    },
+}))(MuiDialogActions);
+
 
 export function CategoryModal() {
     const classes = useStyles();
     const open = useSelector(state => state.misc.catModal);
     const dispatch = useDispatch();
+    const [newBoard, setBoard] = useState({
+    });
 
     const handleClose = () => {
         dispatch(miscActions.closeCategoryModal(false));
     };
+    const handleAddBoard= () => {
+        console.log(newBoard)
+    }
+    function handleChange(e) {
+        const {name, value} = e.target;
+        setBoard(newBoard => ({...newBoard, [event.target.name]: event.target.value}));
+    }
+    console.log(newBoard)
 
     return (
         <div>
-            <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                className={classes.modal}
-                open={open}
-                onClose={handleClose}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                    timeout: 500,
-                }}
-            >
-                <Fade in={open}>
-                    <div className={classes.paper}>
-                        <h2 id="transition-modal-title">Transition modal</h2>
-                        <p id="transition-modal-description">react-transition-group animates me.</p>
-                    </div>
-                </Fade>
-            </Modal>
+            <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open} className={classes.modalWindow}>
+                <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+                    Add a new Board
+                </DialogTitle>
+                <form>
+
+            <DialogContent dividers>
+                    <Typography gutterBottom>
+                        A board heps you to classify and organise your items and entries
+                    </Typography>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        name="name"
+                        label="Title"
+                        type="text"
+                        fullWidth
+                        required
+                        value={newBoard.title}
+                        onChange={handleChange}
+
+                    />
+                    <TextField
+                        id="outlined-textarea"
+                        label="Description"
+                        name="description"
+                        multiline
+                        // variant="outlined"
+                        fullWidth
+                        value={newBoard.description}
+                        onChange={handleChange}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleAddBoard} color="primary" type="submit">
+                        Save changes
+                    </Button>
+                </DialogActions>
+            </form>
+            </Dialog>
         </div>
     );
 }
