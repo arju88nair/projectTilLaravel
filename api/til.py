08b898cb7224b9dca7b958bc3baedb5c3fda287f
Your Name <arju88nair@gmail.com>
2020-09-06 23:33:37 +0530

    changes for server

--- api/til.py
+++ api/til.py
@@ -7,25 +7,36 @@ from flask_jwt_extended import JWTManager
 from resources.routes import initialize_routes
 from database.model import RevokedTokenModel
 from flask_cors import CORS
-
 app = Flask(__name__)
 CORS(app)
 
-app.config.from_envvar('ENV_FILE_LOCATION')
+# app.config.from_envvar('ENV_FILE_LOCATION')
+# app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
+# app.config['MONGODB_SETTINGS'] = environ.get('MONGODB_SETTINGS')
+
 api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
-
+app.config.from_pyfile('env.py')
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
     return RevokedTokenModel.is_jti_blacklisted(jti)
 
+
+app.config['JWT_SECRET_KEY'] = app.config.get("JWT_SECRET_KEY")
+# app.config['MONGODB_SETTINGS'] = app.config.get("MONGODB_SETTINGS")
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/til'
+}
 
-
+print(app.config)
 initialize_db(app)
 initialize_routes(api)
-app.debug = True
-app.run(port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+    app.debug = True
+    app.run(port=5000)
+
