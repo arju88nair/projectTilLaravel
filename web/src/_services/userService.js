12b6dc14e12bbd1a96db0b6db080d3ffc5e4252d
Your Name <arju88nair@gmail.com>
2020-10-03 00:28:23 +0530

    CORS fix

--- web/src/_services/userService.js
+++ web/src/_services/userService.js
@@ -14,7 +14,7 @@ export const userService = {
 function login(user) {
     const requestOptions = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin':'*'},
         body: JSON.stringify(user)
     };
 
