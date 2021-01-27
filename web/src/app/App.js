87f5d6c4952476ae28f4d97ea25f5463a34cdf56
Your Name <arju88nair@gmail.com>
2021-01-28 01:00:02 +0530

    Hero Banner

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,7 +1,7 @@
 import {Router, Route, Switch, Redirect} from 'react-router-dom';
 import {history} from '../_helpers';
 import {LandingPage} from '../views/LandingPage';
-import './App.css';
+import '../resources/css/App.css';
 
 function App() {
   return (
--- web/src/views/LandingPage/LandingPage.jsx
+++ web/src/views/LandingPage/LandingPage.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
+import {LandingAppBar} from './AppBar';
+import {HeroBanner} from './Banner'
 
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     root: {
         flexGrow: 1,
     },
 }));
-export  function LandingPage() {
+
+export function LandingPage() {
     const classes = useStyles();
+
     return (
         <div className={classes.root}>
-hi        </div>
+            <LandingAppBar/>
+            <HeroBanner/>
+        </div>
     );
 }
