33fbdcfa2dd58e57c46e1f6b9e579350b900add7
Your Name <arju88nair@gmail.com>
2021-02-13 23:32:50 +0530

    Action change

--- web/src/views/Components/PreAppBar.js
+++ web/src/views/Components/PreAppBar.js
@@ -14,7 +14,7 @@ import Grid from "@material-ui/core/Grid";
 import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import {useSelector} from "react-redux";
-import {COLORS} from '../../_helpers/';
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles((theme) => ({
     grow: {
@@ -22,50 +22,6 @@ const useStyles = makeStyles((theme) => ({
     },
     menuButton: {
         marginRight: theme.spacing(2),
-    },
-    title: {
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-        maxWidth: 180,
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
     },
     sectionDesktop: {
         display: 'none',
@@ -91,13 +47,13 @@ const useStyles = makeStyles((theme) => ({
         zIndex: theme.zIndex.drawer + 1000,
         color: '#fff',
     },
-    navBar:{
+    navBar: {
         background: 'var(--color-topBar)',
-        alignContent:"center"
     },
-    navBarText:{
+    navBarText: {
         textDecoration: 'none',
-        color: 'var(--color-text)'
+        color: 'var(--color-text)',
+        textTransform: "none"
     },
 }));
 
@@ -152,57 +108,19 @@ export function PreAppBar() {
             onClose={handleMobileMenuClose}
         >
             <MenuItem style={{background: '#fefef9'}}>
-                <NavLink to="/landing"  className={classes.navBarText}>Feed </NavLink>
+                <NavLink to="/landing" className={classes.navBarText}>Feed </NavLink>
             </MenuItem>
             <MenuItem style={{background: '#fefef9'}}>
                 <NavLink to="/landing" className={classes.navBarText}>Blog </NavLink>
             </MenuItem>
             <MenuItem style={{background: '#fefef9'}}>
-                <NavLink to="/register" className={classes.navBarText}>Get Started </NavLink>
+                <NavLink to="/login" className={classes.navBarText}>Login</NavLink>
             </MenuItem>
-            {/*<MenuItem style={{border: "4px solid #457FCA"}}>*/}
-            {/*    <NavLink to="/login" style={{textDecoration: 'none', color: '#1488cc'}}>Log In </NavLink>*/}
-            {/*</MenuItem>*/}
+            <MenuItem style={{background: '#fefef9'}}>
+                <NavLink to="/register" className={classes.navBarText}>Sign up</NavLink>
+            </MenuItem>
         </Menu>
     );
-
-    const renderButtons = () => {
-        switch (location.pathname) {
-
-            case "/login":
-                return <Button
-                    variant="contained"
-                    style={{
-                        "backgroundImage": "linear-gradient(to right, #1488cc, #0076cb, #0062c7, #004cbf, #2b32b2)",
-                        color: 'white', fontWeight: "600"
-                    }}
-                    component={Link} to="/register">
-                    Sign Up
-                </Button>;
-            case "/register":
-                return <Button
-                    variant="contained"
-                    style={{
-                        "backgroundImage": "linear-gradient(to right, #1488cc, #0076cb, #0062c7, #004cbf, #2b32b2)",
-                        color: 'white', fontWeight: "600"
-                    }}
-                    component={Link} to="/login">
-                    Log In
-                </Button>;
-            case "/landing":
-                return <Button
-                    variant="contained"
-                    style={{
-                        "backgroundImage": "linear-gradient(to right, #1488cc, #0076cb, #0062c7, #004cbf, #2b32b2)",
-                        color: 'white', fontWeight: "600"
-                    }}
-                    component={Link} to="/register">
-                    Get started
-                </Button>;
-
-
-        }
-    }
 
     return (
 
@@ -210,24 +128,33 @@ export function PreAppBar() {
             <Backdrop className={classes.backdrop} open={open}>
                 <CircularProgress color="inherit"/>
             </Backdrop>
-            <AppBar position="static" className={classes.navBar} >
+            <AppBar position="static" className={classes.navBar}>
                 <Toolbar>
                     <Grid item className={classes.logoDiv} component={Link} to="/landing">
-                        <img src={logo} alt="logo" className={classes.logo} />
+                        <img src={logo} alt="logo" className={classes.logo}/>
                     </Grid>
                     <div className={classes.grow}/>
                     <div className={classes.sectionDesktop}>
                         <div>
-                            <Button className={classes.navBarText} style={{fontWeight: "600",backgroundColor: 'transparent'}} component={Link} to="/landing">
-                                Feed
-                            </Button>
-                            <Button className={classes.navBarText} style={{fontWeight: "600",backgroundColor: 'transparent'}} component={Link} to="/landing">
+
+                            <Button className={classes.navBarText}
+                                    style={{fontWeight: "600", backgroundColor: 'transparent'}} component={Link}
+                                    to="/landing">
                                 Blog
                             </Button>
-
-                            {renderButtons()}
-
-
+                            <Button className={classes.navBarText}
+                                    style={{fontWeight: "600", backgroundColor: 'transparent'}} component={Link}
+                                    to="/login">
+                                Login
+                            </Button>
+                            <Button variant="contained" className={classes.navBarText}
+                                    style={{
+                                        backgroundColor:"black",
+                                        color: 'white', fontWeight: "600"
+                                    }}
+                                    component={Link} to="/register">
+                                Sign Up
+                            </Button>
                         </div>
                     </div>
                     <div className={classes.sectionMobile}>
@@ -244,7 +171,6 @@ export function PreAppBar() {
                 </Toolbar>
             </AppBar>
             {renderMobileMenu}
-            {renderMenu}
         </div>
     );
 }
